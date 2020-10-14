436b88e2af66d0b7e8478708463799090f2e3b44
IgorGuariroba <1g0r.guari@gmail.com>
2020-10-14 01:57:30 -0300

    #Blog Editar artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -8,7 +8,10 @@ const Article  = require("./Aticle");
 
 router.get("/admin/articles", (req, res) => {
     Article.findAll({
-        include: [{model: Category}]
+        order: [
+            ['updatedAt','DESC']
+        ],
+        include: [{model: Category}] 
     }).then(articles => {
     res.render("admin/articles/index", {articles: articles});
     });
@@ -59,5 +62,42 @@ router.post("/articles/save", (req, res) => {
     });
 });
 
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit",{article: article, categories: categories});
+            });
+            
+        }else{
+            res.redirect("/admin/articles");
+        }
+
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    var id         = req.body.id;
+    var title      = req.body.title;
+    var body       = req.body.body;
+    var categoryId = req.body.category;
+
+    Article.update({title:title, slug: slugify(title), body:body, categoryId:categoryId},{
+        where:{
+            id:id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/admin/articles");
+    });
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -70,7 +70,10 @@ app.use("/",articlesController);
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render("index", {articles: articles});
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+
+        });
     });
  });
 
@@ -83,7 +86,10 @@ app.use("/",articlesController);
         }
     }).then(article => {
         if(article != undefined){
-            res.render("article", {article: article});
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+    
+            });
         }else{
             res.redirect("/");
         }
@@ -92,6 +98,26 @@ app.use("/",articlesController);
     });
  });
 
+
+ app.get("/category/:slug", (req,res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/")
+    });
+ });
 
  app.listen(80,() =>{
      console.log("O servidor está rodando")
